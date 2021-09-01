95260b87de000d02c826e8d8e443822d47c20420
Sebastian <seb.schuette@gmail.com>
2021-09-01 12:03:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,21 +5,21 @@ grid = [
         ['4a', '4b', '4c', '4d', '4e'],
         ['5a', '5b', '5c', '5d', '5e']
         ]
-
+# left to right
 for row in range(len(grid)):
     print('\n')
     for column in range(len(grid[row])):
         print(grid[row][column], end = ' ')
 
 print('\n\n--------------')
-
+# top to bottom
 for column in range(len(grid[row])):
     print('\n')
     for row in range(len(grid)):
         print(grid[row][column], end = ' ')
 
 print('\n\n--------------')
-
+# top left to bottom right
 # 1b = 1 2
 # 2c = 2 3
 # 3d = 3 4
@@ -34,14 +34,16 @@ for row in range(len(grid) - 3):
             print(grid[row + i][column + i], end = ' ')
             
 print('\n\n--------------')
+# top right to bottom left
 # 1e = 1 5
 # 2d = 2 4
 # 3c = 3 3
 for row in range(len(grid) - 3):
+    print('\n')
     for column in grid[row][::-1]: 
 
-        print(column)
-        print(grid[row][column], end = ' ')
+        print(column, end = ' ')
+        # print(grid[row][column], end = ' ')
 
-        for i in range(1, 4):
-            print(grid[row + i][column + i], end = ' ')
+        # for i in range(1, 4):
+        #     print(grid[row + i][column + i], end = ' ')
